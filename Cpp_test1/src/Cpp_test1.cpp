e8a7f8b98660e257f12daf4b0e1d1e352890ba1d
johu <jophuttenhuis@gmail.com>
2014-02-28 13:31:42 +0100

    Initial commit

---
+++ Cpp_test1/src/Cpp_test1.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Cpp_test1.cpp
+// Author      : JoHu
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
